0d6ea0c2816b46652d88bda45d4fe4e902194b67
LuisSoaresBM <luis.soares@bluemetrics.com.br>
2023-02-06 22:39:02 -0300

    Set redirect

--- tribike-frontend/js/user_page.js
+++ tribike-frontend/js/user_page.js
@@ -1,9 +1,7 @@
 window.addEventListener("load", () => {
-    function sendData() {
+
+    function signUp() {
       const XHR = new XMLHttpRequest();
-  
-      // Bind the FormData object and the form element
-      const FD = new FormData(form);
   
       // Define what happens on successful data submission
       XHR.addEventListener("load", (event) => {
@@ -42,14 +40,53 @@ window.addEventListener("load", () => {
       // The data sent is what the user provided in the form
       //XHR.send(FD);
     }
+
+    function login() {
+      const XHR = new XMLHttpRequest();
+  
+      // Define what happens on successful data submission
+      XHR.addEventListener("load", (event) => {
+        //alert('Cadastrado com sucesso!');
+        window.location.replace("./dashboard.html");
+      });
+  
+      // Define what happens in case of error
+      XHR.addEventListener("error", (event) => {
+        alert(target.responseText);
+      });
+  
+      // Set up our request
+      XHR.open("POST", "http://localhost:8082/login");
+      XHR.setRequestHeader("Content-type", "application/json");
+      
+      data='{\"email\": \"';
+      data=data+document.getElementById("login_mail").value;
+      data=data+'\",    \"senha\": \"';
+      data=data+document.getElementById("login_pwd").value;
+      data=data+'\"}';
+      console.log(data);
+      XHR.send(data);
+
+      // The data sent is what the user provided in the form
+      //XHR.send(FD);
+    }
   
     // Get the form element
-    const form = document.getElementById("signup-form");
+    const signup_form = document.getElementById("signup-form");
+    const login_form= document.getElementById("login-form");
   
     // Add 'submit' event handler
-    form.addEventListener("submit", (event) => {
+    signup_form.addEventListener("submit", (event) => {
       event.preventDefault();
-  
-      sendData();
+      if(document.getElementById("sign_pwd").value==document.getElementById("confirm_pwd").value){
+        signUp();
+      }else{
+        alert('As senhas não são iguais!');
+      }
+    });
+
+    login_form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      login();
     });
   });
